9c200e7728d4b177bfc40c2a2dabca9d9a15a6f8
Anthony Broome <anthonybroome87@gmail.com>
2017-04-20 12:38:05 +0100

    rename peer

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network_peering" "primary" {
-  name                          = "primarypeer"
+  name                          = "peer_${var.primary_vpc["vnet_name"]}_${var.secondary_vpc["vnet_name"]}"
   resource_group_name           = "${var.primary_vpc["resource_group_name"]}"
   virtual_network_name          = "${var.primary_vpc["vnet_name"]}"
   remote_virtual_network_id     = "${var.secondary_vpc["vnet_id"]}"
